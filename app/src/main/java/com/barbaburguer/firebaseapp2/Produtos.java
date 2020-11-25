483eafd8d7dd6d5965ff58ed0a5fe5ec9f4dd2b3
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2020-11-25 19:43:24 -0300

    Criada a classe produtas
    Baseado no usuário, foi criado um objeto produtos para inserção no banco de dados

---
+++ app/src/main/java/com/barbaburguer/firebaseapp2/Produtos.java
@@ -0,0 +1,33 @@
+package com.barbaburguer.firebaseapp2;
+
+public class Produtos {
+    private String descricao,marca;
+    private int preco;
+
+    public Produtos() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+}
