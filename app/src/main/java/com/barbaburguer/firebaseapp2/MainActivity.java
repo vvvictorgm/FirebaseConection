9ca4fdf85dd8ce75bb81bb701e2950abc542489f
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2020-11-26 16:25:33 -0300

    Verificação de usuário
    Foi criado um usuário no banco de dados e verificado se o mesmo estava logado no sistema

--- app/src/main/java/com/barbaburguer/firebaseapp2/MainActivity.java
+++ app/src/main/java/com/barbaburguer/firebaseapp2/MainActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,11 +18,36 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
-
+    private FirebaseAuth usuario = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Verificar usuario logado*/
+
+        if(usuario.getCurrentUser() != null){
+            Log.i("CreateUser", "Usuario logado");
+
+        }else{
+            Log.i("CreateUser", "Usuario não logado");
+
+
+        }
+
+        /*Cadastrando um usuario
+        usuario.createUserWithEmailAndPassword("victorfrazatto@hotmail.com","victorjava")
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.i("CreateUser", "Sucesso ao cadastrar usuário");
+                        }else{
+                            Log.i("CreateUser", "Erro ao cadastrar usuário");
+                        }
+                    }
+                });
+
         DatabaseReference usuarios = referencia.child("usuarios");
         DatabaseReference produtos = referencia.child("produtos");
 
